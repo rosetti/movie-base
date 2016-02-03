718eb0c638cd116090e4fa538dc012c8e6d46f28
Vin <Vin@Vin-PC>
2016-02-03 21:02:15 +0000

    Refactoring of the MainWindow class

--- MovieBase/src/gui/MainWindow.java
+++ MovieBase/src/gui/MainWindow.java
@@ -17,80 +17,42 @@ import movieControl.Movie;
 import resources.StaticTestObjects;
 
 public class MainWindow
-{
-  private JFrame frame;
-  private JMenuBar menuBar;
-  private JMenu fileMenu;
-  private Container contentPane;
-  private JPanel mainBox;
-  private JMenuItem addMovies;
+{ 
+	private JFrame frame;
+	private Container contentPane;
+	private SidePanel sidePanel;
+	private ImageIcon logo;
   
-  private SidePanel sidePanel;
+	public MainWindow()
+	{
+		makeFrame();
+		frame.setJMenuBar(new MenuBar().getMenuBar());
+		frame.add(new TopPanel().getPanel(), "North");
+		sidePanel = new SidePanel(StaticTestObjects.getTestMovie2());
+		frame.add(sidePanel.getPanel(), "West");
+		frame.add(new MoviePanel().getPanel(), "Center");
+		frame.setVisible(true);
+	}
   
-  public MainWindow()
-  {
-    makeFrame();
-    addMenuBar();
-    frame.setJMenuBar(this.menuBar);
-    frame.add(new TopPanel().getPanel(), "North");
-    sidePanel = new SidePanel(StaticTestObjects.getTestMovie2());
-    frame.add(sidePanel.getPanel(), "West");
-    frame.add(new MoviePanel().getPanel(), "Center");
-    addMenuItem();
-    frame.setVisible(true);
-  }
+	public void setSidePanel(Movie movie)
+	{
+		sidePanel = new SidePanel(movie);
+		frame.add(sidePanel.getPanel(), "West");
+		frame.revalidate();
+		System.out.println("Side Panel Method finished");
+	}
   
-  public void setSidePanel(Movie movie)
-  {
-	  sidePanel = new SidePanel(movie);
-	  frame.add(sidePanel.getPanel(), "West");
-	  frame.revalidate();
-	  System.out.println("Side Panel Method finished");
-  }
-  //
-  private void makeFrame()
-  {
-    this.frame = new JFrame("Vin's Movie Base");
-    this.frame.setDefaultCloseOperation(3);
-    this.frame.setBackground(Color.black);
-    ImageIcon logo = new ImageIcon("Y:\\Development\\SinSoftMovieApplication\\src\\resources\\MovieBase Logo.png");
-    this.frame.setIconImage(logo.getImage());
-    this.frame.setSize(900, 650);
+	private void makeFrame()
+	{
+		frame = new JFrame("Vin's Movie Base");
+		frame.setDefaultCloseOperation(3);
+		frame.setBackground(Color.black);
+		logo = new ImageIcon("Y:\\Development\\SinSoftMovieApplication\\src\\resources\\MovieBase Logo.png");
+		frame.setIconImage(logo.getImage());
+		frame.setSize(900, 650);
     
-    this.contentPane = this.frame.getContentPane();
-    this.contentPane.setLayout(new BorderLayout());
-  }
+		contentPane = frame.getContentPane();
+		contentPane.setLayout(new BorderLayout());
+	}
   
-  private void testMethod()
-  {
-    System.out.println("Hello Search!");
-  }
-  
-  private void addMenuItem()
-  {
-    this.addMovies = new JMenuItem("Add Movies");
-    
-    //this.addMovies.addActionListener(new MainWindow.1(this));
-    
-    this.fileMenu.add(this.addMovies);
-  }
-  
-  private void addMenuBar()
-  {
-    this.menuBar = new JMenuBar();
-    this.menuBar.setBackground(Theme.topBackground);
-    this.menuBar.add(getMenu());
-  }
-  
-  private void makeMainBox()
-  {
-    this.mainBox = new JPanel();
-    this.mainBox.setBackground(Color.ORANGE);
-  }
-  
-  private JMenu getMenu()
-  {
-    this.fileMenu = new JMenu("File");
-    return this.fileMenu;
-  }
 }
