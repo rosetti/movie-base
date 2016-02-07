0fecebc8b44ffc2924a5920af364854ccf4a8e32
vin <vin372@gmail.com>
2016-02-07 18:51:11 +0000

    added some theme fields

--- MovieBase/src/gui/Theme.java
+++ MovieBase/src/gui/Theme.java
@@ -1,9 +1,13 @@
 package gui;
 
 import java.awt.Color;
+import javax.swing.border.*;
 
 public class Theme
 {
   static Color topBackground = Color.LIGHT_GRAY;
-  static Color mainBackground = Color.DARK_GRAY;
+  static Color mainBackground = Color.LIGHT_GRAY;
+  static Border standardBorder = new LineBorder(Color.DARK_GRAY,4);
+  static Border frameBorder = new LineBorder(Color.BLUE, 3);
+  static Border internalBorder = new LineBorder(Color.darkGray,1);
 }
