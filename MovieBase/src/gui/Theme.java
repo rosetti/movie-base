4a8b3ce540406a45d805d0a247ae5a07570b7cba
Vin <Vin@Vin-PC>
2016-02-09 19:22:17 +0000

    fields added to Theme class

--- MovieBase/src/gui/Theme.java
+++ MovieBase/src/gui/Theme.java
@@ -2,13 +2,21 @@ package gui;
 
 import java.awt.Color;
 import javax.swing.border.*;
+import java.awt.Dimension;
 
 public class Theme
 {
-  static Color topBackground = Color.LIGHT_GRAY;
-  static Color mainBackground = Color.LIGHT_GRAY;
-  static Border standardBorder = new LineBorder(Color.DARK_GRAY,4);
-  static Border frameBorder = new LineBorder(Color.BLUE, 3);
-  static Border internalBorder = new LineBorder(Color.darkGray,1);
-  static Border singleBlackBorder = new LineBorder(Color.black, 1);
+  
+	//Colors
+	static Color topBackground = Color.LIGHT_GRAY;
+	static Color mainBackground = Color.LIGHT_GRAY;
+  
+	//Borders
+	static Border standardBorder = new LineBorder(Color.DARK_GRAY,4);
+	static Border frameBorder = new LineBorder(Color.BLUE, 3);
+	static Border internalBorder = new LineBorder(Color.darkGray,1);
+	static Border singleBlackBorder = new LineBorder(Color.black, 1);
+  
+	//Dimensions
+	static Dimension buttonSize = new Dimension(100,30);
 }
