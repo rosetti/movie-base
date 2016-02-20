eb238b49a2d917ee8e4d49091067a27bee4400d0
vin <vin372@gmail.com>
2016-02-20 22:21:42 +0000

    Add exit option, clear database option, and about menu

--- MovieBase/src/gui/MenuBar.java
+++ MovieBase/src/gui/MenuBar.java
@@ -1,28 +1,32 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+
 
 public class MenuBar 
 {
 	private JMenuBar menuBar;
 	private JMenu fileMenu;
+	private JMenu aboutMenu;
 	
 	public MenuBar()
 	{
+		fileMenu = new JMenu("File");
+		aboutMenu = new JMenu("About");
+		
 		menuBar = new JMenuBar();
 	    menuBar.setBackground(Theme.topBackground);
-	    menuBar.add(getMenu());
+	    menuBar.add(fileMenu);
+	    menuBar.add(aboutMenu);
 	    
-	    addMenuItem();
+	    addFileMenuItems();
+	    addAboutMenuItems();
 	    
-	}
-	
-	public JMenu getMenu()
-	{
-		this.fileMenu = new JMenu("File");
-	    return this.fileMenu;
 	}
 	
 	public JMenuBar getMenuBar()
@@ -30,9 +34,55 @@ public class MenuBar
 		return menuBar;
 	}
 	
-	 private void addMenuItem()
+	 private void addFileMenuItems()
 	  {
 	    JMenuItem addMoviesItem = new JMenuItem("Add Movies");
+	    addMoviesItem.addActionListener(new ActionListener()
+	    	{
+	    		public void actionPerformed(ActionEvent e)
+	    		{
+	    			new ImportWindow();
+	    		}
+	    	});
+	    
+	    JMenuItem clearMoviesItem = new JMenuItem("Clear MovieBase Data");
+	    clearMoviesItem.addActionListener(new ActionListener()
+	    {
+	    	public void actionPerformed(ActionEvent e)
+	    	{
+	    		//message box, sure you wanna clear?
+	    		//if yes, delete all images, and XML file
+	    		//reload mainwindow
+	    	}
+	    });
+	    
+	    JMenuItem exitItem = new JMenuItem("Exit");
+	    exitItem.addActionListener(new ActionListener()
+	    {
+	    	public void actionPerformed(ActionEvent e)
+	    	{
+	    		//exit method
+	    	}
+	    		
+	    });
+	    
+	    fileMenu.add(clearMoviesItem);
 	    fileMenu.add(addMoviesItem);
+	    fileMenu.addSeparator();
+	    fileMenu.add(exitItem);
 	  }
+
+	 private void addAboutMenuItems()
+	 {
+		 JMenuItem aboutItem = new JMenuItem("About");
+		 aboutItem.addActionListener(new ActionListener() 
+		{
+			 public void actionPerformed(ActionEvent e)
+			 {
+				 //open about window
+			 }
+		});
+		 
+		 aboutMenu.add(aboutItem);
+	 }
 }
