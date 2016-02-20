254936958060a52623c8d8601a7ad691ed86d418
vin <vin372@gmail.com>
2016-02-20 22:24:54 +0000

    create a class to control the launching of the program

---
+++ MovieBase/src/main/ProgramLaunch.java
@@ -0,0 +1,42 @@
+package main;
+
+//java imports
+import java.io.File;
+
+//local imports
+import movieControl.*;
+import parsers.LocalParser;
+import gui.*;
+
+public class ProgramLaunch 
+{
+	LocalParser lParser;
+	MovieBase coreBase;
+	
+	public ProgramLaunch()
+	{
+	
+		coreBase = new MovieBase();
+		
+		if (isExistingLibrary())
+		{
+			lParser = new LocalParser(ApplicationMain.pwd + ApplicationMain.slash + "movieData.xml");
+			coreBase = lParser.readMovies(coreBase);
+		}
+		
+		initialiseMainWindow();
+	}
+	
+	private void initialiseMainWindow()
+	{
+		MainWindow mainWindow = new MainWindow();
+		mainWindow.addMoviesToPanel(coreBase.getMovieBase());
+		mainWindow.refreshByResize();
+	}
+	
+	private boolean isExistingLibrary()
+	{
+		File library = new File(ApplicationMain.pwd + ApplicationMain.slash + "movieData.xml");
+		return library.exists();
+	}
+}
