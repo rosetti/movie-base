bfb1a3720092edc1451dd39888ba0b27edecdf57
Vin <Vin@Vin-PC>
2016-08-28 17:10:23 +0100

    JavaFX Views: MainWindow, MenuBar, MovieDetail, Movies

---
+++ MovieBase/src/main/java/javafxgui/MainWindowView.java
@@ -0,0 +1,53 @@
+package javafxgui;
+
+//Java Imports
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.stage.StageStyle;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Background;
+
+//Local Imports
+import javafxgui.MenuBarView; 
+
+public class MainWindowView extends Application
+{
+	Stage mainStage;
+	Scene mainScene;
+	BorderPane mainPane;
+	
+	public void start(Stage primaryStage)
+	{
+		prepScene();
+		prepMainStage(primaryStage);
+		mainStage.show();
+	}
+	
+	private void prepMainStage(Stage primaryStage)
+	{
+		mainStage = primaryStage;
+		mainStage.setTitle("vklx MovieBase");
+		prepDimensions();
+		mainStage.setScene(mainScene);
+	}
+	
+	private void prepDimensions()
+	{
+		mainStage.setHeight(500);
+		mainStage.setWidth(500);
+	}
+	
+	private void prepScene()
+	{
+		mainPane = new BorderPane();
+		
+		mainPane.setTop(new MenuBarView());
+		mainPane.setLeft(new MovieDetailView());
+		mainPane.setCenter(new MoviesView());
+		
+		mainScene = new Scene(mainPane, 500, 500);
+	}
+}
